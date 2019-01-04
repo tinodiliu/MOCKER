26d766ccf47006708ebc89d3f5a0e0455f352e71
tinodiliu <44628035+tinodiliu@users.noreply.github.com>
2019-01-04 20:02:42 +0800

    sdk

---
+++ contrib/NeuronDataReader SDK b18/include/NeuronDataReader-version.h
@@ -0,0 +1,15 @@
+
+#define NEURONDATAREADER_PROJECT_NAME        "NeuronDataReader"
+#define NEURONDATAREADER_PROJECT_DESCRIPTION "Everything realted to NeuronDataReader"
+
+#define NEURONDATAREADER_AUTHOR_ORGANIZATION "NeuronDataReader 1.0"
+#define NEURONDATAREADER_AUTHOR_DOMAIN       "http://www.noitom.com"
+#define NEURONDATAREADER_AUTHOR_MAINTAINER   "contact@noitom.com"
+
+#define NEURONDATAREADER_VERSION_MAJOR       "1"
+#define NEURONDATAREADER_VERSION_MINOR       "1"
+#define NEURONDATAREADER_VERSION_PATCH       "3"
+#define NEURONDATAREADER_VERSION_REVISION    "470"
+
+#define NEURONDATAREADER_VERSION             "1.1.3"
+#define NEURONDATAREADER_NAME_VERSION        "NeuronDataReader v1.1.3 (470)"
